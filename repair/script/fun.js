00e96fcc7139b25504c78cc0602732e7287ee79f
userya <278874876@qq.com>
2014-04-24 09:46:58 +0800

    repair

---
+++ repair/script/fun.js
@@ -0,0 +1,19 @@
+//--
+var imgScrollNum2=new Array();
+for(i=0;i<50;i++){
+  imgScrollNum2[i]=0;	
+}
+function imgScrollRight2(a,b,c,d){
+	    //a.stop();
+		if(imgScrollNum2[d]<b){
+			imgScrollNum2[d]++;
+			a.animate({scrollLeft: imgScrollNum2[d]*c}, 200);
+			}
+	}	
+function imgScrollLeft2(a,b,c,d){
+	    //a.stop();
+		if(imgScrollNum2[d]>0){
+			imgScrollNum2[d]--;
+			a.animate({scrollLeft: imgScrollNum2[d]*c}, 200);		
+			}
+	}	
