3ac8d712b5673f6d459877e1077f4342243353be
userya <278874876@qq.com>
2014-04-26 00:13:23 +0800

    框架搭建，课程团队框架

--- bar/menu.js
+++ bar/menu.js
@@ -1,81 +1,47 @@
 var menuData = {
-	"kctd": 
-	{
-		"title": "课程团队",
-		"sub": 
-		[
-			{
-				"title": "主讲教师",
-				"show":true,
-				"sub": 
-				[
-					{
-						"title": "a",
-						"clicked":true,
-						"url": "#/kctd/aaa"
-					},
-
-					{
-						"title": "b",
-						"url": ""
-					}
-				]
-			},
-			{
-				"title": "AAAAAAAAA",
-				"show":true,
-				"sub": 
-				[
-					{
-						"title": "aaa",
-						"url": ""
-					},
-					{
-						"title": "addd",
-						"url": ""
-					}
-				]
-			}
-		]
+	"kctd" : {
+		"title" : "课程团队",
+		"sub" : [ {
+			"title" : "主讲教师",
+			"show" : true,
+			"url" : "#/kctd/aaa"
+		}, {
+			"title" : "课程建设团队",
+			"show" : false,
+			"url" : "#/kctd/aaa"
+		}, {
+			"title" : "企业专家",
+			"show" : false,
+			"url" : "#/kctd/aaa"
+		} ]
 	},
-	"jxwj": 
-	{
-		"title": "课程团队",
-		"sub": 
-		[
-			{
-				"title": "主讲教师",
-				"show":true,
-				"sub": 
-				[
-					{
-						"title": "",
-						"url": ""
-					},
-
-					{
-						"title": "",
-						"url": ""
-					}
-				]
-			},
-
-			{
-				"title": "",
-				"show":true,
-				"sub": 
-				[
-					{
-						"title": "",
-						"url": ""
-					},
-
-					{
-						"title": "",
-						"url": ""
-					}
-				]
-			}
-		]
+	"jxwj" : {
+		"title" : "教学文件",
+		"sub" : [ {
+			"title" : "专业教学实施方案",
+			"show" : true,
+			"url" : "#/kctd/aaa"
+		}, {
+			"title" : "授课计划",
+			"url" : ""
+		}, {
+			"title" : "课程标准",
+			"url" : ""
+		}, {
+			"title" : "管理制度",
+			"sub" : [ {
+				"title" : "教学常规管理",
+				"url" : ""
+			}, {
+				"title" : "教学质量监控",
+				"url" : ""
+			}, {
+				"title" : "实训室管理",
+				"url" : ""
+			}, {
+				"title" : "实习管理",
+				"url" : ""
+			} ]
+		} ]
 	}
 };
