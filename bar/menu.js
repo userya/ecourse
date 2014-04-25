5b46e8f0dde4fe7b376ebe162b96ff74ab7f4a2c
userya <278874876@qq.com>
2014-04-25 17:18:37 +0800

    add

---
+++ bar/menu.js
@@ -0,0 +1,81 @@
+var menuData = {
+	"kctd": 
+	{
+		"title": "课程团队",
+		"sub": 
+		[
+			{
+				"title": "主讲教师",
+				"show":true,
+				"sub": 
+				[
+					{
+						"title": "a",
+						"clicked":true,
+						"url": "#/kctd/aaa"
+					},
+
+					{
+						"title": "b",
+						"url": ""
+					}
+				]
+			},
+			{
+				"title": "AAAAAAAAA",
+				"show":true,
+				"sub": 
+				[
+					{
+						"title": "aaa",
+						"url": ""
+					},
+					{
+						"title": "addd",
+						"url": ""
+					}
+				]
+			}
+		]
+	},
+	"jxwj": 
+	{
+		"title": "课程团队",
+		"sub": 
+		[
+			{
+				"title": "主讲教师",
+				"show":true,
+				"sub": 
+				[
+					{
+						"title": "",
+						"url": ""
+					},
+
+					{
+						"title": "",
+						"url": ""
+					}
+				]
+			},
+
+			{
+				"title": "",
+				"show":true,
+				"sub": 
+				[
+					{
+						"title": "",
+						"url": ""
+					},
+
+					{
+						"title": "",
+						"url": ""
+					}
+				]
+			}
+		]
+	}
+};
